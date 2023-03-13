4013cabee627808481e0d374f1cc04cb5ba5c6c0
Trí Thức <kawaiiqw.123@gmail.com>
2023-03-13 14:23:15 +0700

    Trang 25

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -30,6 +30,7 @@ namespace BigSchool.Controllers
         }
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
             if (!ModelState.IsValid)
